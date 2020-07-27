efca1e06267fa47ce58cafd3e4c157b8c469de88
catcherochek <catcherochek@yandex.ru>
2020-07-27 13:25:29 +0300

    first commit

---
+++ admin/language/ru-ru/blog/article.php
@@ -0,0 +1,46 @@
+<?php
+// Heading
+$_['heading_title']          = 'Артикли';
+
+// Text
+$_['text_success']           = 'Успешно: Артикул изменен удачно';
+$_['text_list']              = 'Список артикулей';
+$_['text_add']               = 'Добавить артикул';
+$_['text_edit']              = 'Изменить артикул';
+$_['text_default']           = 'По умолчанию';
+$_['text_extension']         = 'Расширение';
+$_['text_plus']              = '+';
+$_['text_minus']             = '-';
+
+// Column
+$_['column_name']            = 'Имя артикула';
+$_['column_author']          = 'Article Author';
+$_['column_status']          = 'Status';
+$_['column_action']          = 'Action';
+
+// Entry
+$_['entry_name']             = 'Article Name';
+$_['entry_store']             = 'Stores';
+$_['entry_image']            = 'Blog Image';
+$_['entry_author']           = 'Author';
+$_['entry_description']      = 'Description';
+$_['entry_meta_title'] 	     = 'Meta Tag Title';
+$_['entry_meta_keyword'] 	 = 'Meta Tag Keywords';
+$_['entry_meta_description'] = 'Meta Tag Description';
+$_['entry_intro_text']       = 'Introduction Text';
+$_['entry_keyword']          = 'SEO Keyword';
+$_['entry_status']           = 'Status';
+$_['entry_sort_order']       = 'Sort Order';
+$_['entry_category']         = 'Categories';
+
+// Help
+$_['help_keyword']           = 'Do not use spaces, instead replace spaces with - and make sure the keyword is globally unique.';
+$_['help_category']          = '(Autocomplete)';
+
+// Error
+$_['error_warning']          = 'Warning: Please check the form carefully for errors!';
+$_['error_permission']       = 'Warning: You do not have permission to modify articles!';
+$_['error_name']             = 'Article Name must be greater than 3 and less than 255 characters!';
+$_['error_meta_title']       = 'Meta Title must be greater than 3 and less than 255 characters!';
+$_['error_keyword']          = 'SEO keyword already in use!';
+
